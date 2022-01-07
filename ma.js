b318df37a46b385e73bb45bbce0ba3e246575c40
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-01-07 10:23:03 +0100

    done

---
+++ ma.js
@@ -0,0 +1,17 @@
+var firstVar; // empty var
+var name = "Michael"; // first name
+var number = 27; // 27
+var division = 20 / 5; // 4
+var frog = "frog"; // frog string
+var orderHasShipped = true; // true
+
+if (orderHasShipped === true) {
+  console.log("true"); // true
+} else {
+  console.log("false"); // false
+}
+
+for (var i = 0; i <= 9; i++) {
+  // will go from 0 - 9
+  console.log(i);
+}
